3bb7adeb7902fcceae67e30d1f8f5f19c76a59e1
Tenzin_Tsongi_lama <np03cs4a220340@heraldcollege.edu.np>
2023-02-11 22:33:42 +0545

    complete

---
+++ week9/scipt.js
@@ -0,0 +1,7 @@
+/*let username = window.prompt("Are you gay: ");
+console.log(username);*/
+
+document.getElementById("myButton").onclick = function(){
+let username = document.getElementById("myText").value;
+console.log(username);
+}
